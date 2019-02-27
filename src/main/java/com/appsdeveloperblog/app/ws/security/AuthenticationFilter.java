b3d342829040bedd1bf525471f99adb923717018
ershad <ershad@gmail.com>
2019-02-27 12:47:31 +0600

    Polishing

--- src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/AuthenticationFilter.java
@@ -45,7 +45,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException {
         String userName = ((User) auth.getPrincipal()).getUsername();
-        //        String tokenSecret = SecurityConstants.TOKEN_SECRET
         String token = Jwts.builder()
                 .setSubject(userName)
                 .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
--- src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -23,7 +23,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().addFilter(new AuthenticationFilter(authenticationManager()));
+
     }
 
     @Override
